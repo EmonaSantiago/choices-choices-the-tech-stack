93e6a2bf90a2c728d9c690bffc7651ba08348540
Emona Santiago <emonasantiago1207@gmail.com>
2022-10-30 16:23:19 +0100

    c

--- CMD-MIDTERM-VUE/src/App.vue
+++ CMD-MIDTERM-VUE/src/App.vue
@@ -9,5 +9,54 @@ export default {
 </script>
 
 <template>
-   <h1>{{ message }}</h1>
+   <!-- <h1>{{ message }}</h1> -->
+    <header> 
+      <h1>CMD MIDTERM</h1>
+      <nav class="navigation">
+          <a href="#home" class="">Beoogde leerresultaten</a>
+          <a href="#band" class="">Onderwijsleeromgeving</a>
+          <a href="#tour" class="">Toetsing</a>
+          <a href="#contact" class="">Gerealiseerde leerresultaten</a>
+        </nav>
+
+      <section id="hero-section">
+        <p></p>
+      </section>
+    </header>
+  
+   <main>
+    <section id="intro">
+      <p>hbjavenlwfjk;qm</p>
+    </section>
+
+   </main>
+
 </template>
+
+<style>
+  body{
+    color: black;
+  }
+
+  header {
+    border: 10px solid black;
+  }
+  .navigation a {
+    font-size: 18px;
+    margin: 1em;
+  }
+
+  .navigation {
+    background-color: white;
+    color: rgb(0, 0, 0);
+    height: 6em;
+    text-align: right;
+  }
+
+  main {
+    background-color: #FFF021;
+    height: 30em;
+  }
+  
+
+</style>
