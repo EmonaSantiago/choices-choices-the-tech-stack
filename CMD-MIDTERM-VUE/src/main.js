2139e17e3515a16b4ae04d2afd972635afb717ee
Emona Santiago <emonasantiago1207@gmail.com>
2022-11-08 12:43:43 +0100

    .

--- CMD-MIDTERM-VUE/src/main.js
+++ CMD-MIDTERM-VUE/src/main.js
@@ -5,3 +5,19 @@ import "./assets/main.css";
 import "./components/Header.vue";
 
 createApp(App).mount("#app");
+
+const routes = [
+  // will match everything and put it under `$route.params.pathMatch`
+  { path: "/:pathMatch(.*)*", name: "NotFound", component: NotFound },
+  // will match anything starting with `/user-` and put it under `$route.params.afterUser`
+  { path: "/user-:afterUser(.*)", component: UserGeneric },
+];
+
+this.$router.push({
+  name: "NotFound",
+  // preserve current path and remove the first char to avoid the target URL starting with `//`
+  params: { pathMatch: this.$route.path.substring(1).split("/") },
+  // preserve existing query and hash if any
+  query: this.$route.query,
+  hash: this.$route.hash,
+});
